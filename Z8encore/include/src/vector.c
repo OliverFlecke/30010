cce0dbd5ce0fc4e9db716aa68a0029026f8eef42
Oliver Fleckenstein <Oliverfl@live.dk>
2015-06-09 16:01:41 +0200

    Added vector

---
+++ Z8encore/include/src/vector.c
@@ -0,0 +1,10 @@
+
+
+struct TVector {
+	long x,y;
+}
+
+void initVector(struct TVector *v) {
+	v->x = 10;
+	v->y = 20;
+}
---
+++ Z8encore/include/vector.h
@@ -0,0 +1,6 @@
+#ifndef TVECTOR_H
+#define TVECTOR_H
+
+void initVector(struct TVector *v) 
+
+#endif
