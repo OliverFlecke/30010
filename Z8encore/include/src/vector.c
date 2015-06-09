1b170105a09e6afe3eece4da34b68cb7db469c11
Oliver Fleckenstein <Oliverfl@live.dk>
2015-06-09 16:11:15 +0200

    Updated vector

--- Z8encore/include/src/vector.c
+++ Z8encore/include/src/vector.c
@@ -1,10 +1,24 @@
-
-
+// #include "vector.h"
 struct TVector {
 	long x,y;
-}
+};
 
 void initVector(struct TVector *v) {
-	v->x = 10;
-	v->y = 20;
+	v->x = 1;
+	v->y = 2;
+}
+
+void rotate(struct TVector *v, signed short phi) {
+	long temp = v->x * cos(phi) - v->y * sin(phi);
+	v->y = v->y * sin(phi) + v->x * cos(phi);
+	v->x = temp;
+}
+
+void main() {
+	struct TVector v;
+	initVector(&v);
+
+	printf("%d %d\n", v.x, v.y);
+
+	rotate(&v, 180);
 }
